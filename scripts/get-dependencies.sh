1ad0cd14a9fe74a14980171bbac6c8edcab0ab7c
Oleg Balunenko <obalunenko@luno.com>
2019-11-17 02:28:10 +0300

    scripts: Fix scripts; Add CI linters run

--- scripts/get-dependencies.sh
+++ scripts/get-dependencies.sh
@@ -1,26 +1,22 @@
 #!/usr/bin/env bash
 
 function get_dependencies() {
-    declare -a packages=(
-        "golang.org/x/tools/cmd/cover/..."
-        "github.com/mattn/goveralls/..."
-        "github.com/Bubblyworld/gogroup/..."
-        "golang.org/x/lint/golint/..."
-    )
+  declare -a packages=(
+    "golang.org/x/tools/cmd/cover/..."
+    "github.com/mattn/goveralls/..."
+    "github.com/Bubblyworld/gogroup/..."
+    "golang.org/x/lint/golint/..."
+  )
 
-    ## now loop through the above array
-    for pkg in "${packages[@]}"
-    do
-        echo "$pkg"
-        go get -u -v "$pkg"
-    done
+  ## now loop through the above array
+  for pkg in "${packages[@]}"; do
+    echo "$pkg"
+    go get -u -v "$pkg"
+  done
 
-    curl -sfL https://install.goreleaser.com/github.com/golangci/golangci-lint.sh | sh -s -- -b $(go env GOPATH)/bin
-    golangci-lint --version
+  curl -sfL https://install.goreleaser.com/github.com/golangci/golangci-lint.sh | sh -s -- -b $(go env GOPATH)/bin
+  golangci-lint --version
 }
-
-
-
 
 echo Gonna to update go tools and packages...
 get_dependencies
