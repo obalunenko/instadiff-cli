532e82f5db8262c69b7146f761ad77575f0ee747
Oleg Balunenko <oleg.balunenko@gmail.com>
2021-02-11 17:32:53 +0300

    move client to separate file

--- internal/service/service.go
+++ internal/service/service.go
@@ -5,15 +5,11 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"os"
-	"path/filepath"
-	"strings"
 	"sync"
 	"time"
 
 	"github.com/TheForgotten69/goinsta/v2"
 	log "github.com/sirupsen/logrus"
-	"github.com/tcnksm/go-input"
 
 	"github.com/obalunenko/instadiff-cli/internal/config"
 	"github.com/obalunenko/instadiff-cli/internal/db"
@@ -114,163 +110,6 @@ func New(ctx context.Context, cfg config.Config, cfgPath string) (*Service, Stop
 	}
 
 	return &svc, stopFunc, nil
-}
-
-func makeClient(cfg config.Config, cfgPath string) (*goinsta.Instagram, error) {
-	var cl *goinsta.Instagram
-
-	uname, err := username()
-	if err != nil {
-		return nil, fmt.Errorf("username: %w", err)
-	}
-
-	pwd, err := password()
-	if err != nil {
-		return nil, fmt.Errorf("password: %w", err)
-	}
-
-	sessFile := filepath.Join(cfgPath, fmt.Sprintf("%s.sess", uname))
-
-	if i, err := goinsta.Import(sessFile); err == nil {
-		log.Infof("session imported from file: %s", sessFile)
-
-		cl = i
-
-		return i, nil
-	}
-
-	cl = goinsta.New(uname, pwd)
-
-	if err := cl.Login(); err != nil {
-		var chErr *goinsta.ChallengeError
-
-		if !errors.As(err, &chErr) {
-			return nil, fmt.Errorf("failed to login: %w", err)
-		}
-
-		cl, err = challenge(cl, chErr.Challenge.APIPath)
-		if err != nil {
-			return nil, fmt.Errorf("challenge: %w", err)
-		}
-	}
-
-	if cfg.StoreSession() {
-		if err := cl.Export(sessFile); err != nil {
-			log.Errorf("save session: %v", err)
-		}
-	}
-
-	return cl, nil
-}
-
-// ErrEmptyInput returned in case when user input is empty.
-var ErrEmptyInput = errors.New("should not be empty")
-
-func username() (string, error) {
-	ui := &input.UI{
-		Writer: os.Stdout,
-		Reader: os.Stdin,
-	}
-
-	name, err := ui.Ask("What is your username?",
-		&input.Options{
-			Default:     "",
-			Loop:        true,
-			Required:    true,
-			HideDefault: false,
-			HideOrder:   false,
-			Hide:        false,
-			Mask:        false,
-			MaskDefault: false,
-			MaskVal:     "",
-			ValidateFunc: func(s string) error {
-				s = strings.TrimSpace(s)
-				if s == "" {
-					return ErrEmptyInput
-				}
-
-				return nil
-			},
-		})
-	if err != nil {
-		return "", fmt.Errorf("username input: %w", err)
-	}
-
-	return name, nil
-}
-
-func password() (string, error) {
-	ui := &input.UI{
-		Writer: os.Stdout,
-		Reader: os.Stdin,
-	}
-
-	pwd, err := ui.Ask("What is your password?",
-		&input.Options{
-			Default:     "",
-			Loop:        true,
-			Required:    true,
-			HideDefault: false,
-			HideOrder:   false,
-			Hide:        false,
-			Mask:        false,
-			MaskDefault: false,
-			MaskVal:     "",
-			ValidateFunc: func(s string) error {
-				s = strings.TrimSpace(s)
-				if s == "" {
-					return ErrEmptyInput
-				}
-
-				return nil
-			},
-		})
-	if err != nil {
-		return "", fmt.Errorf("password input: %w", err)
-	}
-
-	return pwd, nil
-}
-
-func challenge(cl *goinsta.Instagram, chURL string) (*goinsta.Instagram, error) {
-	if err := cl.Challenge.Process(chURL); err != nil {
-		return nil, fmt.Errorf("process challenge: %w", err)
-	}
-
-	ui := &input.UI{
-		Writer: os.Stdout,
-		Reader: os.Stdin,
-	}
-
-	code, err := ui.Ask("What is SMS code for instagram?",
-		&input.Options{
-			Default:     "",
-			Loop:        true,
-			Required:    true,
-			HideDefault: false,
-			HideOrder:   false,
-			Hide:        false,
-			Mask:        false,
-			MaskDefault: false,
-			MaskVal:     "",
-			ValidateFunc: func(s string) error {
-				s = strings.TrimSpace(s)
-				if s == "" {
-					return ErrEmptyInput
-				}
-
-				return nil
-			},
-		})
-	if err != nil {
-		return nil, fmt.Errorf("process input: %w", err)
-	}
-
-	if err = cl.Challenge.SendSecurityCode(code); err != nil {
-		return nil, fmt.Errorf("send security code: %w", err)
-	}
-
-	return cl, nil
 }
 
 // GetFollowers returns list of followers for logged in user.
