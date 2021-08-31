58c44740de8ec95f27a0da0cfbe4e16727fb9830
Oleg Balunenko <oleg.balunenko@gmail.com>
2021-08-31 17:48:25 +0300

    chore: Update ci

--- scripts/release.sh
+++ scripts/release.sh
@@ -9,4 +9,6 @@ git fetch --tags
 latestTag=$(git describe --tags $(git rev-list --tags --max-count=1))
 echo "${latestTag}"
 
+export GOVERSION=$(go version | awk '{print $3;}')
+
 curl -sL https://git.io/goreleaser | bash
