43e05fc14f11ff2ac8a9cfbda001716eaa7cc23a
Oleg Balunenko <oleg.balunenko@gmail.com>
2022-09-14 00:39:43 +0300

    fix: Fix default value usage for config path flag (#170)
    * fix: Fix default value usage for config path flag
    
    * style: gofmt

--- cmd/instadiff-cli/flags.go
+++ cmd/instadiff-cli/flags.go
@@ -9,25 +9,30 @@ func globalFlags() []cli.Flag {
 		&cli.StringFlag{
 			Name:     cfgPath,
 			Usage:    "Path to the config file",
-			Required: true,
+			Required: false,
 			Value:    ".config.json",
 		},
 		&cli.StringFlag{
-			Name:  logLevel,
-			Usage: "Level of output logs",
-			Value: "INFO",
+			Name:     logLevel,
+			Usage:    "Level of output logs",
+			Required: false,
+			Value:    "INFO",
 		},
 		&cli.BoolFlag{
-			Name:  incognito,
-			Usage: "Incognito removes session on application exit.",
+			Name:     incognito,
+			Usage:    "Incognito removes session on application exit.",
+			Required: false,
+			Value:    false,
 		},
 	}
 }
 
 func addListFlag() *cli.BoolFlag {
 	return &cli.BoolFlag{
-		Name:  list,
-		Usage: "Print the full list instead of only number",
+		Name:     list,
+		Usage:    "Print the full list instead of only number",
+		Required: false,
+		Value:    false,
 	}
 }
 
