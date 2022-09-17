4b94574ed57685a2c2d1c2a49938ce382b5f1b83
Oleg Balunenko <oleg.balunenko@gmail.com>
2022-09-17 03:00:39 +0300

    feat: Order list  by following date - earlier first

--- internal/client/instagram/instagram.go
+++ internal/client/instagram/instagram.go
@@ -317,7 +317,7 @@ func (i *Client) Followers(ctx context.Context) ([]models.User, error) {
 
 // Followings returns list of followings.
 func (i *Client) Followings(ctx context.Context) ([]models.User, error) {
-	return makeUsersList(ctx, i.client.Account.Following("", goinsta.LatestOrder))
+	return makeUsersList(ctx, i.client.Account.Following("", goinsta.EarliestOrder))
 }
 
 // Username returns current account username.
