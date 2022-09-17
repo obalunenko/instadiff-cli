ddd06925376e61de937d63eebed10cd68bdcde31
Oleg Balunenko <oleg.balunenko@gmail.com>
2022-09-18 01:00:57 +0300

    fix: Use latest order for user's followings

--- internal/client/instagram/instagram.go
+++ internal/client/instagram/instagram.go
@@ -317,7 +317,7 @@ func (i *Client) Followers(ctx context.Context) ([]models.User, error) {
 
 // Followings returns list of followings.
 func (i *Client) Followings(ctx context.Context) ([]models.User, error) {
-	return makeUsersList(ctx, i.client.Account.Following("", goinsta.EarliestOrder))
+	return makeUsersList(ctx, i.client.Account.Following("", goinsta.LatestOrder))
 }
 
 // Username returns current account username.
@@ -414,10 +414,6 @@ func makeUsersList(ctx context.Context, users *goinsta.Users) ([]models.User, er
 			return nil, fmt.Errorf("users iterate: %w", err)
 		}
 	}
-
-	// sort.Slice(usersList, func(i, j int) bool {
-	//	return usersList[i].ID > usersList[j].ID
-	// })
 
 	return usersList, nil
 }
