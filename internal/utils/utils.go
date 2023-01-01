697cbcc0c7e03809eb6aec290390c70d1ce4a7fe
Oleg Balunenko <oleg.balunenko@gmail.com>
2023-01-01 18:46:57 +0400

    feat: Implement log error closure helper func

---
+++ internal/utils/utils.go
@@ -0,0 +1,14 @@
+package utils
+
+import (
+	"context"
+
+	log "github.com/obalunenko/logger"
+)
+
+// LogError helper for closure funcs error handling.
+func LogError(ctx context.Context, err error, msg string) {
+	if err != nil {
+		log.WithError(ctx, err).Error(msg)
+	}
+}
